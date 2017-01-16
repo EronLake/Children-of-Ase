5300b72e52d838b81557378dc0cf8189cfa33926
Justin Patrick Farris <jpfarris@crm.ucsc.edu>
2017-01-16 12:00:58 -0800

    Transferring player movement to Engine branch

---
+++ Children of Osi/Children of Osi/movement.cpp
@@ -0,0 +1,35 @@
+#pragma comment(lib, "user32.lib")
+#include "common.h"
+#include "movement.h"
+#include "Player.h"
+#include <Windows.h>
+
+using namespace std;
+
+void movePlayer(Player player) {
+	if (GetAsyncKeyState(VK_UP)) {
+		player.shiftY(-.01);
+		cout << "\nX: " << player.getX() << " Y: " << player.getY();
+	}
+	if (GetAsyncKeyState(VK_DOWN)) {
+		player.shiftY(.1);
+		cout << "\nX: " << player.getX() << " Y: " << player.getY();
+	}
+	if (GetAsyncKeyState(VK_LEFT)) {
+		player.shiftX(-.1);
+		cout << "\nX: " << player.getX() << " Y: " << player.getY();
+	}
+	if (GetAsyncKeyState(VK_RIGHT)) {
+		player.shiftX(.1);
+		cout << "\nX: " << player.getX() << " Y: " << player.getY();
+	}
+}
+
+int main() {
+	bool loopGame = true;
+	Player shango(10, 10, true);
+	while (loopGame) {
+		movePlayer(shango);
+	}
+
+}
