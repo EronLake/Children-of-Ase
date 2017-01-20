c0377eb067958b0496792d0743f7f3452b66deb2
Alexander <alexander.ou@gmail.com>
2017-01-20 01:28:35 -0800

    added code to check collision for bounding boxes

--- ChildrenOfOsi/ChildrenOfOsi/Main.cpp
+++ ChildrenOfOsi/ChildrenOfOsi/Main.cpp
@@ -35,42 +35,9 @@
 using namespace std;
 
 
-void testQuadTree() {
-	Rectangle* testrec = new Rectangle(Vector2f(0.0, 0.0), 100.0, 100.0);	//init screen
-	Rectangle* Alex = new Rectangle(Vector2f(51.0,51.0),20.0,20.0);	//init player
-	vector<Rectangle*> recVec;	//obj vector
-
-	for (int i = 0; i < 100; i++) {		//init obj vec
-		Rectangle* myRec = new Rectangle(Vector2f(rand() % 90, rand() % 90), 10.0, 10.0);
-		recVec.push_back(myRec);
-	}
-	cout << "before making tree\n";
-
-	QuadTree* myTree = new QuadTree(0, testrec);	//init 
-	cout << "after making tree\n";
-	bool mybool = true;
-	while (mybool) {	//tick every frame
-		cout << "before clear" << endl;
-		myTree->clear();	//clear my tree
-		cout << "after clear" << endl;
-		for (int i = 0; i < recVec.size(); i++) {
-			cout << "before insert" << endl;
-			myTree->insert(recVec[i]);	//insert all obj into tree
-			cout << "after insert" << endl;
-		}
-		vector<Rectangle*> collidable;	
-		myTree->retrieve(collidable, Alex);	//vector now holds all collidable obj to Alex
-		cout << "after retrieve" << endl;
-		int count = 0;
-		for (int i = 0; i < collidable.size(); i++) {
-			count++;
-			cout << "this is a collidable obj number " << i << endl;
-		}
-		cout << "total number of collidable obj is " << count << endl;
-		mybool = false;
-	}
-	//system("PAUSE");
-}
+void testQuadTree();
+bool checkCollision(Rectangle *recA, Rectangle *recB);	//given two bounding boxes, check if they collide
+bool coordOverlap(int value, int min, int max) { return (value >= min) && (value <= max); }		//helper func for checkCollision
 
 
 int main() {
@@ -119,20 +86,69 @@ int main() {
 	//Alex: test QT tomorrow on hero obj, and make QT generic to take in any obj
 	testQuadTree();
 
-	Texture test;
-	test.setFrames(5);
-	test.setFWidth(100);
-	Sprite sp;
-	sp.setTexture(&test);
-	test.setAnimated(true);
-	person.setSprite(sp);
-	for (int i = 0; i < 20;i++) {
-		person.animateObj();
-	}
+	//Texture test;
+	//test.setFrames(5);
+	//test.setFWidth(100);
+	//Sprite sp;
+	//sp.setTexture(&test);
+	//test.setAnimated(true);
+	//person.setSprite(sp);
+	//for (int i = 0; i < 20;i++) {
+	//	person.animateObj();
+	//}
 	_CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_DEBUG);
 	_CrtDumpMemoryLeaks();
 
 	return 0;
+}
+
+void testQuadTree()
+{
+	Rectangle* testrec = new Rectangle(Vector2f(0.0, 0.0), 100.0, 100.0);	//init screen
+	Rectangle* Alex = new Rectangle(Vector2f(51.0, 51.0), 20.0, 20.0);	//init player
+	vector<Rectangle*> recVec;	//obj vector
+
+	for (int i = 0; i < 100; i++) {		//init obj vec
+		Rectangle* myRec = new Rectangle(Vector2f(rand() % 90, rand() % 90), 10.0, 10.0);
+		recVec.push_back(myRec);
+	}
+	//cout << "before making tree\n";
+
+	QuadTree* myTree = new QuadTree(0, testrec);	//init 
+	//cout << "after making tree\n";
+	bool mybool = true;
+	while (mybool) {	//tick every frame
+		//cout << "before clear" << endl;
+		myTree->clear();	//clear my tree
+		//cout << "after clear" << endl;
+		for (int i = 0; i < recVec.size(); i++) {
+			//cout << "before insert" << endl;
+			myTree->insert(recVec[i]);	//insert all obj into tree
+			//cout << "after insert" << endl;
+		}
+		vector<Rectangle*> collidable;
+		myTree->retrieve(collidable, Alex);	//vector now holds all collidable obj to Alex
+		//cout << "after retrieve" << endl;
+		//int count = 0;
+		for (int i = 0; i < collidable.size(); i++) {
+			if (checkCollision(collidable[i], Alex)) {
+				cout << "collision between Alex and obj number " << i << endl;
+				cout << "Alex width and height are " << Alex->getWidth() << ", " << Alex->getHeight() << endl;
+				cout << "obj width and height are " << collidable[i]->getWidth() << ", " << collidable[i]->getHeight() << endl;
+				cout << "Alex's bounds are " << Alex->getX() << ", " << Alex->getY() << endl;
+				cout << "Obj bounds are " << collidable[i]->getX() << ", " << collidable[i]->getY() << endl;
+			}
+		}
+		mybool = false;
+	}
+	system("PAUSE");
+}
+
+bool checkCollision(Rectangle *recA, Rectangle *recB)
+{
+	bool xCollide = coordOverlap(recA->getX(), recB->getX(), recB->getX() + recB->getWidth()) || coordOverlap(recB->getX(), recA->getX(), recA->getX() + recA->getWidth());
+	bool yCollide = coordOverlap(recA->getY(), recB->getY(), recB->getY() + recB->getHeight()) || coordOverlap(recB->getY(), recA->getY(), recA->getY() + recA->getHeight());
+	return xCollide && yCollide;
 }
 
 
