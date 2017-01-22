68d11569508b93ae1ec64422e5c93d6e8697f02e
darkapb <anpbarr@ucsc.edu>
2017-01-21 18:00:07 -0800

    Delete SoundSystem.h

--- SoundSystem.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-typedef FMOD::Sound* SoundClass;
-class SoundSystemClass {
-public:
-	SoundSystemClass();
-	void createSound(SoundClass *pSound, const char* pFile);
-	void playSound(SoundClass pSound, bool bLoop = false);
-	void releaseSound(SoundClass pSound);
-};
