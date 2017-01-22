86461d910b7b7263a529d5674d245fa3db382a4e
Daniel Hildebrandt <daniel.s.hildebrandt+git@gmail.com>
2017-01-21 21:54:29 -0800

    Added GameWindow code. Needs testing, adjustment.

---
+++ ChildrenOfOsi/ChildrenOfOsi/OpenGL Shaders/StdVertexShader.vert.glsl
@@ -0,0 +1,15 @@
+# version 330 core
+
+layout (location = 0) in vec3 iPosition;
+layout (location = 1) in vec3 iColor;
+layout (location = 2) in vec2 iTextureCoords;
+
+out vec3 oColor;
+out vec2 oTextureCoords;
+
+void main()
+{
+  gl_position = vec4(iPosition, 1.0F);
+  oColor = iColor;
+  oTextureCoords = oTextureCoords;
+}
