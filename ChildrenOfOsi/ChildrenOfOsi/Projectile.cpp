091d5ce89a3e0cdfa086bd2b1f9bbe3a79c92998
acalba93 <acalba@ucsc.edu>
2017-01-23 13:30:20 -0800

    Added projectile class and game object filter
    Added projectile class and game object filter

---
+++ ChildrenOfOsi/ChildrenOfOsi/Projectile.cpp
@@ -0,0 +1,21 @@
+#include "common.h"
+#include "Projectile.h"
+
+
+Projectile::Projectile()
+{
+}
+
+Projectile::Projectile(float x, float y, bool col, int d) : WorldObj(x, y, col)
+{
+	dmg = d;
+}
+
+Projectile::~Projectile()
+{
+}
+
+ void Projectile::Hit(LivingObj *target)
+{
+	 (*target).addHealth(-dmg);
+}
---
+++ ChildrenOfOsi/ChildrenOfOsi/Projectile.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "WorldObj.h"
+#include "LivingObj.h"
+
+class Projectile :
+	public WorldObj
+{
+public:
+	Projectile();
+	Projectile(float x, float y, bool col, int d);
+	~Projectile();
+	void Hit(LivingObj *target);
+private:
+	int dmg;
+};
+
