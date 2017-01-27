82f303f0187ede94cc0af4f82ee32db0a61ef6c2
darkapb <anpbarr@ucsc.edu>
2017-01-26 16:55:55 -0800

    Working Sound Engine done without SoundObject Functionality

---
+++ ChildrenOfOsi/ChildrenOfOsi/SoundSystem.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "SoundObject.h"
+typedef FMOD::Sound* SoundClass;
+#include <vector>
+class SoundSystemClass {
+public:
+	SoundSystemClass();
+	void createSound(SoundClass *pSound, const char* pFile);
+	void playSound(SoundClass pSound, bool bLoop, FMOD::Channel* channel, bool ispaused);
+	void releaseSound(SoundClass pSound);
+	//void playSoundObject(SoundObject sound);
+	//void changeSoundSource(SoundObject sound, const char* name);
+	//FMOD::Channel *channels[32];
+};
--- ChildrenOfOsi/ChildrenOfOsi/testmain.cpp
+++ ChildrenOfOsi/ChildrenOfOsi/testmain.cpp
@@ -1,32 +1,51 @@
-#include "stdafx.h"
 using namespace std;
+
 
 #include <cstdio>
 #include <iostream>
-//#include "fmod.hpp"
+#include "fmod.hpp"
 #include "SoundSystem.h"
+#include "SoundObject.h"
+#include "soundType.h"
+#include <time.h>
 
-/*typedef FMOD::Sound* SoundClass;
+typedef FMOD::Sound* SoundClass;
 int main()
 {
+	FMOD::Channel *channels[32]; //all of the channels
+
+	foo::soundType type;
+	FMOD::Sound* soundSample;
+	const char* name;              //the variables required
+	FMOD::Channel* channel;
+	bool ispaused;
+	unsigned int time;
 	
 	// Initialize our sound system
-	SoundSystemClass sound;
+	SoundSystemClass soundsystem;
 	
+	type = foo::soundType::music;
+	name = "04.wav";
+	channel = channels[0];//assign the channel
+	ispaused = false;
+	
+	soundsystem.createSound(&soundSample, name);// Create the sound
+	soundSample->getLength(&time, FMOD_TIMEUNIT_PCM);// Find the length
 
-	// Create a sample sound
-	SoundClass soundSample;
-	sound.createSound(&soundSample, "04.wav"); //soundfile must be in the same directory as the project file
-	 
-	// Play the sound, with loop mode
-	sound.playSound(soundSample, false);
+	//SoundObject* playable = new SoundObject("04.wav", &soundSample, type);
+	//object stuff
+	
+	
+	soundsystem.playSound(soundSample, false,channel,ispaused); 	// Play the sound, with loop mode
 
-	// Do something meanwhile...
-	cout << "Press return to quit." << endl;
+	
+	cout << "Press return to quit." << endl;  // Do something meanwhile...
 	cin.get();
 
-	// Release the sound
+	soundsystem.releaseSound(soundSample); // Release the sound
+	
+	
 	
 
-	sound.releaseSound(soundSample);
-}*/
+	
+}
