a2266db26fb6600acbd86d0ad758580905dfa84b
acalba93 <acalba@ucsc.edu>
2017-02-06 11:34:28 -0800

    floats to ints
    floats to ints

--- ChildrenOfOsi/ChildrenOfOsi/Sprite.cpp
+++ ChildrenOfOsi/ChildrenOfOsi/Sprite.cpp
@@ -17,7 +17,7 @@ void Sprite::setTexture(Texture *t)
 	tex = t;
 	index = 0;
 	start = 0;
-	stop = (*tex).getFWidth();
+	stop = (*tex).getFrameWidth();
 }
 
 void Sprite::animate() 
@@ -25,14 +25,14 @@ void Sprite::animate()
 	if ((*tex).getAnimated()) {
 		if (index < (*tex).getFrames()) {
 			index++;
-			start = (*tex).getFWidth()*(float)index;
-			stop = (*tex).getFWidth()*(float)(index + 1);
+			start = (*tex).getFrameWidth()*index;
+			stop = (*tex).getFrameWidth()*(index + 1);
 			cout << "Animated start: " << start << " stop: " << stop << endl;
 		}
 		else {
 			index = 0;
 			start = 0;
-			stop = (*tex).getFWidth();
+			stop = (*tex).getFrameWidth();
 		}
 	}
 }
