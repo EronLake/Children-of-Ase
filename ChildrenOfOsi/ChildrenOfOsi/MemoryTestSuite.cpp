07712db552170bdede9877a570b5bc9c83d960e1
EronLake <ejlake@ucsc.edu>
2017-02-08 10:47:31 -0800

    added a testSuites folder and memory test suite(stil needs to be filled out)

---
+++ ChildrenOfOsi/ChildrenOfOsi/MemoryTestSuite.cpp
@@ -0,0 +1,14 @@
+#include "stdafx.h"
+#include "MemoryTestSuite.h"
+
+
+MemoryTestSuite::MemoryTestSuite()
+{
+	LOG("MemoryTestSuite Object Constructed");
+}
+
+
+MemoryTestSuite::~MemoryTestSuite()
+{
+	LOG("MemoryTestSuite Object Destroyed");
+}
---
+++ ChildrenOfOsi/ChildrenOfOsi/MemoryTestSuite.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "common.h"
+
+class MemoryTestSuite
+{
+public:
+	MemoryTestSuite();
+	~MemoryTestSuite();
+};
+
