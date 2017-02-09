01559d3128ac04a654965cb931f423654eebbd17
Daniel Hildebrandt <daniel.s.hildebrandt+git@gmail.com>
2017-02-09 11:49:39 -0800

    Cleaned up Texture class, added some convenient constructors.

--- ChildrenOfOsi/ChildrenOfOsi/Texture.cpp
+++ ChildrenOfOsi/ChildrenOfOsi/Texture.cpp
@@ -1,104 +1,28 @@
 #include "stdafx.h"
 #include "Texture.h"
-vector<GLuint> Texture::textureId;
 
-Texture::Texture()
-{
-}
+std::vector<GLuint> Texture::textureId;
 
-
-Texture::~Texture()
-{
-}
-
-float Texture::getWidth()
-{
-	return width;
-}
-
-void Texture::setWidth(int w)
-{
-	width = w;
-}
-
-float Texture::getHeight()
-{
-	return height;
-}
-
-void Texture::setHeight(int h)
-{
-	height = h;
-}
-
-float Texture::getFrameWidth()
-{
-	return frameWidth;
-}
-
-void Texture::setFrameWidth(int w)
-{
-	frameWidth = w;
-}
-
-int Texture::getFrames()
-{
-	return frames;
-}
-
-void Texture::setFrames(int f)
-{
-	frames = f;
-	frameWidth = width / frames;
-}
-
-bool Texture::getAnimated()
-{
-	return animated;
-}
-
-void Texture::setAnimated(bool a)
-{
-	animated=a;
-}
-
-string Texture::getFile()
-{
-	return imageFile;
-}
-
-void Texture::setFile(string img)
-{
-	imageFile = img;
-}
-
-unsigned char* Texture::getImage()
-{
-	return image;
-}
-
+/**
+ * Loads in the image data corresponding to this texture's filename.
+ */
 void Texture::load()
 {
-	txId = textureId.size()+1;
-	image = SOIL_load_image(imageFile.c_str(), &width, &height, 0, SOIL_LOAD_RGBA);
-	textureId.push_back(txId);
-	glGenTextures(1, &textureId[txId-1]);
-	glBindTexture(GL_TEXTURE_2D, textureId[txId-1]);
+  Texture::textureId.push_back(this->txId = Texture::textureId.size() + 1);
+	this->image = SOIL_load_image(imageFile.c_str(), &(this->width), &(this->height), 0, SOIL_LOAD_RGBA);
+
+	glGenTextures(1, &Texture::textureId[txId - 1]);
+	glBindTexture(GL_TEXTURE_2D, Texture::textureId[txId - 1]);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-	image = SOIL_load_image(imageFile.c_str(), &width, &height, 0, SOIL_LOAD_RGBA);
+	image = SOIL_load_image(imageFile.c_str(), &(this->width), &(this->height), 0, SOIL_LOAD_RGBA);
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
 	glGenerateMipmap(GL_TEXTURE_2D);
 	SOIL_free_image_data(image);
 	glBindTexture(GL_TEXTURE_2D, 0);
-}
-
-int Texture::getId()
-{
-	return txId;
 }
