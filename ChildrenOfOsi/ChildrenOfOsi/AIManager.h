e432f15d695220bcc326281360a51cf902070e43
U-DESKTOP-IJO7N67\BlackMagicianDTT <dtoney@ucsc.edu>
2017-02-09 17:14:05 -0800

    Added AIManager(basically just a physics manager clone.

---
+++ ChildrenOfOsi/ChildrenOfOsi/AIManager.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "stdafx.h"
+#include "common.h"
+#include "Task.h"
+#include "MessageLog.h"
+#include "TaskBuffer.h"
+#include "Manager.h"
+
+class AIManager : public Manager
+{
+private:
+	typedef int (Movement::*fn_ptr)(WorldObj*);
+	typedef std::map<std::string, fn_ptr> functionMapper;
+	functionMapper task_map;
+	Movement* moveHelper;
+	Vector2f destination;
+
+
+public:
+	AIManager(MessageLog* _mLog, TaskBuffer* _tBuffer);
+	AIManager(MessageLog* _mLog, TaskBuffer* _tBuffer, QuadTree * _physicsQuadTree);
+	~AIManager();
+
+	virtual void register_manager() final;
+	virtual void execute_task(Task* current_task) final;
+
+};
+
+
+
+
