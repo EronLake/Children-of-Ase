1c599a2b4433146d3f12679077b2080a93ba6b27
acalba93 <acalba@ucsc.edu>
2017-02-13 15:23:27 -0800

    skeleton of dialogue controller
    skeleton of dialogue controller

--- ChildrenOfOsi/ChildrenOfOsi/DialogueController.h
+++ ChildrenOfOsi/ChildrenOfOsi/DialogueController.h
@@ -1,17 +1,30 @@
 #pragma once
 #include "stdafx.h"
 #include "common.h"
+#include "Player.h"
 
 class DialogueController
 {
 public:
 	DialogueController();
 	~DialogueController();
-	void PlayerConversationPoint();
-	void PlayerInform();
-	void PlayerResponse();
+	void PlayerChoose();
+	void PlayerConversationPoint(std::string info);
+	void PlayerResponse(std::string info);
 	void NPCConversationPoint();
-	void NPCResponse();
-
+	void NPCResponse(std::string info);
+	void setPlayer(Player* p);
+	void startConversation(NPC* n,bool playerTalk);
+	NPC* getNPC();
+private:
+	Player* player;
+	NPC* npc;
+	int state;
+	//States:
+	//0 is no conversation
+	//1 is waiting for player input
+	//2 is waiting for player response
+	//3 is npc conversation point
+	//4 is npc response
 };
 
