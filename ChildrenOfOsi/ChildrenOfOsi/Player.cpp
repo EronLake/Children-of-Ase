fc2136520093d74a6848b75d2d1c3b4df8d455c0
acalba93 <acalba@ucsc.edu>
2017-02-16 17:13:01 -0800

    added rectangle to player for dialogue
    we shouldnt push broken code

--- ChildrenOfOsi/ChildrenOfOsi/Player.cpp
+++ ChildrenOfOsi/ChildrenOfOsi/Player.cpp
@@ -15,3 +15,9 @@ Player::Player(float x, float y, bool col):Hero(x,y,col)
 Player::~Player()
 {
 }
+
+{
+	talk.setX((*this).getX()-dist);
+	talk.setY((*this).getY()-dist);
+	talk.setWidth((*this).getWidth()+(2*dist));
+	talk.setHeight((*this).getHeight()+(2*dist));
--- ChildrenOfOsi/ChildrenOfOsi/Player.h
+++ ChildrenOfOsi/ChildrenOfOsi/Player.h
@@ -8,5 +8,6 @@ public:
 	Player();
 	Player(float x, float y, bool col);
 	~Player();
+private:
 };
 
