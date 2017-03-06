392ac685f70857ae00b8831e55b2a98bef4d89d0
Andrew <anpbarr@ucsc.edu>
2017-03-05 17:10:35 -0800

    planner

--- ChildrenOfOsi/ChildrenOfOsi/Action.cpp
+++ ChildrenOfOsi/ChildrenOfOsi/Action.cpp
@@ -76,3 +76,13 @@ int Action::exeAction() {
 	}
 	return 0;
 }
+void Action::setMultipliers(int a, int k, int h, int p, int r, int e, int g) {
+	multipliers = new Personality();
+	multipliers->setAggression(a);
+	multipliers->setKindness(k);
+	multipliers->setHonor(h);
+	multipliers->setPride(p);
+	multipliers->setRecklessness(r);
+	multipliers->setExtroversion(e);
+	multipliers->setGreed(g);
+};
--- ChildrenOfOsi/ChildrenOfOsi/Planner.cpp
+++ ChildrenOfOsi/ChildrenOfOsi/Planner.cpp
@@ -6,7 +6,13 @@ Planner::Planner() {
 Planner::Planner(Hero* hero) {
 	evaluateHero = hero;
 };
-int Planner::personality_appeal(Action* action) {
-
-	return 0;
+int Planner::personality_appeal(Action* evaluateAction) {
+	
+	return ((evaluateHero->traits->getHonor()*evaluateAction->multipliers->getHonor())+
+		(evaluateHero->traits->getPride()*evaluateAction->multipliers->getPride())+
+		(evaluateHero->traits->getAggression()*evaluateAction->multipliers->getAggression())+
+		(evaluateHero->traits->getKindness()*evaluateAction->multipliers->getKindness())+
+		(evaluateHero->traits->getGreed()*evaluateAction->multipliers->getGreed())+
+		(evaluateHero->traits->getRecklessness()*evaluateAction->multipliers->getRecklessness())+
+		(evaluateHero->traits->getExtroversion()*evaluateAction->multipliers->getExtroversion()));
 };
