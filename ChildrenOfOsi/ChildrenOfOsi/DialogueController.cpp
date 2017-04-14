34fafb8cb16b79861d657c123c1a545163e238da
Andrew <anpbarr@ucsc.edu>
2017-04-14 07:40:13 -0700

    added personality and relationship stuff

--- ChildrenOfOsi/ChildrenOfOsi/DialogueController.cpp
+++ ChildrenOfOsi/ChildrenOfOsi/DialogueController.cpp
@@ -128,7 +128,7 @@ void DialogueController::PlayerResponse()
 
 void DialogueController::otherConversationPoint(dialogue_point line)
 {
-	dialogue_point point = dialogue.choose_conv_pt(conversation_log_obj_pointer_vec, optionsIndex);
+	dialogue_point point = dialogue.choose_conv_pt(conversation_log_obj_pointer_vec, yemoja_personality, yemoja_relationship_with_Shango);
 	replyString = point[1];
 
 	Hero* temp_hero = nullptr;
