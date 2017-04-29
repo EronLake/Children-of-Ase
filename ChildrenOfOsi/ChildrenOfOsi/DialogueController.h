2b123d485a02edf90c724ca4db61f55e06a49cfc
U-DESKTOP-IJO7N67\BlackMagicianDTT <dtoney@ucsc.edu>
2017-04-29 16:30:49 -0700

    Added function to offer quest at the end of dialogue.

--- ChildrenOfOsi/ChildrenOfOsi/DialogueController.h
+++ ChildrenOfOsi/ChildrenOfOsi/DialogueController.h
@@ -40,6 +40,7 @@ public:
 	static void add_hero_related_conv_points();
 	static void remove_hero_related_conv_points();
 	static void replace_all(std::string& str, const std::string& from, const std::string& to);
+	static bool offer_quest_on_exit(Hero* hero);
 
 	static bool prompted_quest;
 	static bool accepted_quest;
