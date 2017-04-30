5087c472c7523ce9fe417e397ddbbb03035653c1
Alexander <alexander.ou@gmail.com>
2017-04-30 02:59:34 -0700

    uniform grid added

--- ChildrenOfOsi/ChildrenOfOsi/PhysicsManager.h
+++ ChildrenOfOsi/ChildrenOfOsi/PhysicsManager.h
@@ -17,7 +17,7 @@ private:
 
 public:
 	PhysicsManager(MessageLog* _mLog, TaskBuffer* _tBuffer);
-	PhysicsManager(MessageLog* _mLog, TaskBuffer* _tBuffer, QuadTree* _physicsQuadTree, RiverObj* _rivObj);
+	PhysicsManager(MessageLog* _mLog, TaskBuffer* _tBuffer, QuadTree* _physicsQuadTree, UniformGrid* _uniformGrid, RiverObj* _rivObj);
 	~PhysicsManager();
 
 	virtual void register_manager() final;
