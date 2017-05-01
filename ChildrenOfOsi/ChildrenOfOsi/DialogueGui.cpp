bd05b04679b87a226ca6aeaac2d38acbcff29864
Daniel Hildebrandt <daniel.s.hildebrandt+git@gmail.com>
2017-05-01 00:21:16 -0700

    Intermediate commit. Working on new dialogue UI.

--- ChildrenOfOsi/ChildrenOfOsi/DialogueGui.cpp
+++ ChildrenOfOsi/ChildrenOfOsi/DialogueGui.cpp
@@ -1,201 +1,218 @@
 #include "stdafx.h"
 #include "DialogueGui.h"
 
-
 DialogueGui::DialogueGui()
 {
-	//init coord of all the icons
-	background = new Rectangle(Vector2f(70, 270), 820, 250);
-	swordIcon = new Rectangle(Vector2f(142, 302), 50, 47);
-	heartIcon = new Rectangle(Vector2f(142, 349), 50, 47);
-	faceIcon = new Rectangle(Vector2f(142, 394), 50, 47);
-	questionIcon = new Rectangle(Vector2f(142, 439), 50, 47);
-	dialogueBox = new Rectangle(Vector2f(254, 303), 600, 80);
-	responseBox1 = new Rectangle(Vector2f(280, 390), 544, 90);
-	portrait1 = new Rectangle(Vector2f(100, 70), 300, 400);
-	portrait2 = new Rectangle(Vector2f(700, 70), 300, 400);
-	backgroundTex = new Texture();
-	swordTex = new Texture();
-	swordGlowTex = new Texture();
-	heartTex = new Texture();
-	heartGlowTex = new Texture();
-	faceTex = new Texture();
-	faceGlowTex = new Texture();
-	questionTex = new Texture();
-	questionGlowTex = new Texture();
-	dialogueBoxTex = new Texture();
-	responseBoxTex = new Texture();
-	black = glm::vec3(0, 0, 0);
-	red = glm::vec3(50, 0, 0);
-	DialogueController::scroll_control = 0;
+  this->background_rect = new Rectangle({DialogueGui::BACKGROUND_X, DialogueGui::BACKGROUND_Y}, DialogueGui::BACKGROUND_WIDTH, DialogueGui::BACKGROUND_HEIGHT);
+  this->header_rect = new Rectangle({DialogueGui::HEADER_X, DialogueGui::HEADER_Y}, DialogueGui::HEADER_WIDTH, DialogueGui::HEADER_HEIGHT);
+  this->icon_sword_rect = new Rectangle({DialogueGui::ICON_SWORD_X, DialogueGui::ICON_SWORD_Y}, DialogueGui::ICON_WIDTH, DialogueGui::ICON_HEIGHT);
+  this->icon_heart_rect = new Rectangle({DialogueGui::ICON_HEART_X, DialogueGui::ICON_HEART_Y}, DialogueGui::ICON_WIDTH, DialogueGui::ICON_HEIGHT);
+  this->icon_face_rect = new Rectangle({DialogueGui::ICON_FACE_X, DialogueGui::ICON_FACE_Y}, DialogueGui::ICON_WIDTH, DialogueGui::ICON_HEIGHT);
+  this->icon_question_rect = new Rectangle({DialogueGui::ICON_QUESTION_X, DialogueGui::ICON_QUESTION_Y}, DialogueGui::ICON_WIDTH, DialogueGui::ICON_HEIGHT);
+  this->speaker_left_rect = new Rectangle({0, 0}, 0, 0);
+  this->speaker_right_rect = new Rectangle({0, 0}, 0, 0);
 
+  this->background_tex = new Texture();
+  this->header_tex = new Texture();
+  this->icon_sword_tex = new Texture();
+  this->icon_heart_tex = new Texture();
+  this->icon_face_tex = new Texture();
+  this->icon_question_tex = new Texture();
+  this->speaker_left_tex = new Texture();
+  this->speaker_right_tex = new Texture();
+
+  background = new Rectangle(Vector2f(70, 270), 820, 250);
+  swordIcon = new Rectangle(Vector2f(142, 302), 50, 47);
+  heartIcon = new Rectangle(Vector2f(142, 349), 50, 47);
+  faceIcon = new Rectangle(Vector2f(142, 394), 50, 47);
+  questionIcon = new Rectangle(Vector2f(142, 439), 50, 47);
+  dialogueBox = new Rectangle(Vector2f(254, 303), 600, 80);
+  responseBox1 = new Rectangle(Vector2f(280, 390), 544, 90);
+  portrait1 = new Rectangle(Vector2f(100, 70), 300, 400);
+  portrait2 = new Rectangle(Vector2f(700, 70), 300, 400);
+
+  backgroundTex = new Texture();
+  swordTex = new Texture();
+  swordGlowTex = new Texture();
+  heartTex = new Texture();
+  heartGlowTex = new Texture();
+  faceTex = new Texture();
+  faceGlowTex = new Texture();
+  questionTex = new Texture();
+  questionGlowTex = new Texture();
+  dialogueBoxTex = new Texture();
+  responseBoxTex = new Texture();
+
+  text_color_default = glm::vec3(0, 0, 0);
+  text_color_selected = glm::vec3(50, 0, 0);
+  DialogueController::scroll_control = 0;
 }
 
 
-DialogueGui::~DialogueGui()
-{
-}
+DialogueGui::~DialogueGui() {}
 
 void DialogueGui::loadTexture()
 {
-	//setFile, load, setFrame
-	backgroundTex->setFile("DGBackground.png",1);
-	swordTex->setFile("DGSword.png",1);
-	swordGlowTex->setFile("DGGlowSword.png", 1);
-	heartTex->setFile("DGHeart.png", 1);
-	heartGlowTex->setFile("DGGlowHeart.png", 1);
-	faceTex->setFile("DGFace.png", 1);
-	faceGlowTex->setFile("DGGlowFace.png", 1);
-	questionTex->setFile("DGQuestion.png", 1);
-	questionGlowTex->setFile("DGGlowQuestion.png", 1);
-	dialogueBoxTex->setFile("DGDiaBox.png", 1);
-	responseBoxTex->setFile("DGResponseBox.png", 1);
+  //setFile, load, setFrame
+  backgroundTex->setFile("DGBackground.png", 1);
+  swordTex->setFile("DGSword.png", 1);
+  swordGlowTex->setFile("DGGlowSword.png", 1);
+  heartTex->setFile("DGHeart.png", 1);
+  heartGlowTex->setFile("DGGlowHeart.png", 1);
+  faceTex->setFile("DGFace.png", 1);
+  faceGlowTex->setFile("DGGlowFace.png", 1);
+  questionTex->setFile("DGQuestion.png", 1);
+  questionGlowTex->setFile("DGGlowQuestion.png", 1);
+  dialogueBoxTex->setFile("DGDiaBox.png", 1);
+  responseBoxTex->setFile("DGResponseBox.png", 1);
 }
 
 void DialogueGui::setSprite()
 {
-	background->sprite.setTexture(backgroundTex);
-	swordIcon->sprite.setTexture(swordGlowTex);
-	heartIcon->sprite.setTexture(heartTex);
-	faceIcon->sprite.setTexture(faceTex);
-	questionIcon->sprite.setTexture(questionTex);
-	dialogueBox->sprite.setTexture(dialogueBoxTex);
-	responseBox1->sprite.setTexture(responseBoxTex);
+  background->sprite.setTexture(backgroundTex);
+  swordIcon->sprite.setTexture(swordGlowTex);
+  heartIcon->sprite.setTexture(heartTex);
+  faceIcon->sprite.setTexture(faceTex);
+  questionIcon->sprite.setTexture(questionTex);
+  dialogueBox->sprite.setTexture(dialogueBoxTex);
+  responseBox1->sprite.setTexture(responseBoxTex);
 }
 
 void DialogueGui::setPortrait1(Sprite portrait1Sprite)
 {
-	portrait1->sprite = portrait1Sprite;
+  portrait1->sprite = portrait1Sprite;
 }
 
 void DialogueGui::setPortrait2(Sprite portrait2Sprite)
 {
-	portrait2->sprite = portrait2Sprite;
+  portrait2->sprite = portrait2Sprite;
 }
 
 void DialogueGui::setSwordGlow()
 {
-	heartIcon->sprite.setTexture(heartTex);
-	faceIcon->sprite.setTexture(faceTex);
-	questionIcon->sprite.setTexture(questionTex);
-	swordIcon->sprite.setTexture(swordGlowTex);
+  heartIcon->sprite.setTexture(heartTex);
+  faceIcon->sprite.setTexture(faceTex);
+  questionIcon->sprite.setTexture(questionTex);
+  swordIcon->sprite.setTexture(swordGlowTex);
 }
 
 void DialogueGui::setHeartGlow()
 {
-	swordIcon->sprite.setTexture(swordTex);
-	faceIcon->sprite.setTexture(faceTex);
-	questionIcon->sprite.setTexture(questionTex);
-	heartIcon->sprite.setTexture(heartGlowTex);
+  swordIcon->sprite.setTexture(swordTex);
+  faceIcon->sprite.setTexture(faceTex);
+  questionIcon->sprite.setTexture(questionTex);
+  heartIcon->sprite.setTexture(heartGlowTex);
 }
 
 void DialogueGui::setFaceGlow()
 {
-	heartIcon->sprite.setTexture(heartTex);
-	swordIcon->sprite.setTexture(swordTex);
-	questionIcon->sprite.setTexture(questionTex);
-	faceIcon->sprite.setTexture(faceGlowTex);
+  heartIcon->sprite.setTexture(heartTex);
+  swordIcon->sprite.setTexture(swordTex);
+  questionIcon->sprite.setTexture(questionTex);
+  faceIcon->sprite.setTexture(faceGlowTex);
 }
 
 void DialogueGui::setQuestionGlow()
 {
-	heartIcon->sprite.setTexture(heartTex);
-	faceIcon->sprite.setTexture(faceTex);
-	swordIcon->sprite.setTexture(swordTex);
-	questionIcon->sprite.setTexture(questionGlowTex);
+  heartIcon->sprite.setTexture(heartTex);
+  faceIcon->sprite.setTexture(faceTex);
+  swordIcon->sprite.setTexture(swordTex);
+  questionIcon->sprite.setTexture(questionGlowTex);
 }
 
 void DialogueGui::drawGui()
 {
-	GameWindow::drawSprite(background->getX(), background->getY(), background->getWidth(), background->getHeight(), background->sprite);
-	GameWindow::drawSprite(swordIcon->getX(), swordIcon->getY(), swordIcon->getWidth(), swordIcon->getHeight(), swordIcon->sprite);
-	GameWindow::drawSprite(heartIcon->getX(), heartIcon->getY(), heartIcon->getWidth(), heartIcon->getHeight(), heartIcon->sprite);
-	GameWindow::drawSprite(faceIcon->getX(), faceIcon->getY(), faceIcon->getWidth(), faceIcon->getHeight(), faceIcon->sprite);
-	GameWindow::drawSprite(questionIcon->getX(), questionIcon->getY(), questionIcon->getWidth(), questionIcon->getHeight(), questionIcon->sprite);
-	GameWindow::drawSprite(dialogueBox->getX(), dialogueBox->getY(), dialogueBox->getWidth(), dialogueBox->getHeight(), dialogueBox->sprite);
-	GameWindow::drawSprite(responseBox1->getX(), responseBox1->getY(), responseBox1->getWidth(), responseBox1->getHeight(), responseBox1->sprite);
-	std::string message = DialogueController::getMessage();
+  GameWindow::drawSprite(background->getX(), background->getY(), background->getWidth(), background->getHeight(), background->sprite);
+  GameWindow::drawSprite(swordIcon->getX(), swordIcon->getY(), swordIcon->getWidth(), swordIcon->getHeight(), swordIcon->sprite);
+  GameWindow::drawSprite(heartIcon->getX(), heartIcon->getY(), heartIcon->getWidth(), heartIcon->getHeight(), heartIcon->sprite);
+  GameWindow::drawSprite(faceIcon->getX(), faceIcon->getY(), faceIcon->getWidth(), faceIcon->getHeight(), faceIcon->sprite);
+  GameWindow::drawSprite(questionIcon->getX(), questionIcon->getY(), questionIcon->getWidth(), questionIcon->getHeight(), questionIcon->sprite);
+  GameWindow::drawSprite(dialogueBox->getX(), dialogueBox->getY(), dialogueBox->getWidth(), dialogueBox->getHeight(), dialogueBox->sprite);
+  GameWindow::drawSprite(responseBox1->getX(), responseBox1->getY(), responseBox1->getWidth(), responseBox1->getHeight(), responseBox1->sprite);
+  std::string message = DialogueController::getMessage();
 
-	GameWindow::createText(message, 266, 303, 500, 80, black);
-	std::vector<std::vector<std::string>> options;
-	
-	if (DialogueController::getState() == 1) {
-		options = DialogueController::getOptions();
+  GameWindow::createText(message, 266, 303, 500, 80, text_color_default);
+  std::vector<std::vector<std::string>> options;
 
-		for (int i = 0; i <= 4; i++) {
-			if ( options.size() <= (DialogueController::scroll_control + i))
-				break;
-			std::string option_str = replace_str_char(options[DialogueController::scroll_control + i][1], "_", ' ');
-			if (option_str.find("Advise To",0) != string::npos || option_str.find("Ask About",0) != string::npos)
-				option_str += (" " + options[DialogueController::scroll_control + i][3]);
-			if (DialogueController::getSelect() == i) {
-				GameWindow::createText(option_str, 292, 390 + (18 * i), 544, 45, red);
-			}
-			else {
-				GameWindow::createText(option_str, 292, 390 + (18 * i), 544, 45, black);
-			}
+  if(DialogueController::getState() == 1) {
+    options = DialogueController::getOptions();
 
-		}
-	}
-	if (DialogueController::getState() == 2) {
-		options = DialogueController::getReplyOptions();
+    for(int i = 0; i <= 4; i++) {
+      if(options.size() <= (DialogueController::scroll_control + i))
+        break;
+      std::string option_str = replace_str_char(options[DialogueController::scroll_control + i][1], "_", ' ');
+      if(option_str.find("Advise To", 0) != string::npos || option_str.find("Ask About", 0) != string::npos)
+        option_str += (" " + options[DialogueController::scroll_control + i][3]);
+      if(DialogueController::getSelect() == i) {
+        GameWindow::createText(option_str, 292, 390 + (18 * i), 544, 45, text_color_selected);
+      }
+      else {
+        GameWindow::createText(option_str, 292, 390 + (18 * i), 544, 45, text_color_default);
+      }
 
-		for (int i = 0; i <= 4; i++) {
-			if (options.size() <= (DialogueController::scroll_control + i))
-				break;
-			std::string option_str = replace_str_char(options[DialogueController::scroll_control + i][1], "_", ' ');
-			if (option_str.find("Take Advice",0) != string::npos || option_str.find("Tell About",0) != string::npos) {
-					option_str += (" " + options[DialogueController::scroll_control + i][3]);
-			}
-			if (DialogueController::getSelect() == i) {
-			GameWindow::createText(option_str, 292, 390 + (18 * i), 544, 45, red);
-			}
-			else {
-				GameWindow::createText(option_str, 292, 390 + (18 * i), 544, 45, black);
-			}
+    }
+  }
+  if(DialogueController::getState() == 2) {
+    options = DialogueController::getReplyOptions();
 
-		}
-	}
-	if (DialogueController::getState() == 5) {
-		GameWindow::createText("Next", 292, 390 + (18 * 1), 544, 45, red);
-	}
-	if (DialogueController::getState() == 6) {
-		GameWindow::createText("Next", 292, 390 + (18 * 1), 544, 45, red);
-	}
-	if (DialogueController::getState() == 7) {
-		GameWindow::createText("Exit", 292, 390 + (18 * 1), 544, 45, red);
-	}
-	if (DialogueController::getState() == 8) {
-		GameWindow::createText("Next", 292, 390 + (18 * 1), 544, 45, red);
-	}
-	if (DialogueController::getState() == 9) {
-		GameWindow::createText("Exit", 292, 390 + (18 * 1), 544, 45, red);
-	}
-	if (DialogueController::getState() == 10) {
-		GameWindow::createText("Next", 292, 390 + (18 * 1), 544, 45, red);
-	}
+    for(int i = 0; i <= 4; i++) {
+      if(options.size() <= (DialogueController::scroll_control + i))
+        break;
+      std::string option_str = replace_str_char(options[DialogueController::scroll_control + i][1], "_", ' ');
+      if(option_str.find("Take Advice", 0) != string::npos || option_str.find("Tell About", 0) != string::npos) {
+        option_str += (" " + options[DialogueController::scroll_control + i][3]);
+      }
+      if(DialogueController::getSelect() == i) {
+        GameWindow::createText(option_str, 292, 390 + (18 * i), 544, 45, text_color_selected);
+      }
+      else {
+        GameWindow::createText(option_str, 292, 390 + (18 * i), 544, 45, text_color_default);
+      }
+
+    }
+  }
+  if(DialogueController::getState() == 5) {
+    GameWindow::createText("Next", 292, 390 + (18 * 1), 544, 45, text_color_selected);
+  }
+  if(DialogueController::getState() == 6) {
+    GameWindow::createText("Next", 292, 390 + (18 * 1), 544, 45, text_color_selected);
+  }
+  if(DialogueController::getState() == 7) {
+    GameWindow::createText("Exit", 292, 390 + (18 * 1), 544, 45, text_color_selected);
+  }
+  if(DialogueController::getState() == 8) {
+    GameWindow::createText("Next", 292, 390 + (18 * 1), 544, 45, text_color_selected);
+  }
+  if(DialogueController::getState() == 9) {
+    GameWindow::createText("Exit", 292, 390 + (18 * 1), 544, 45, text_color_selected);
+  }
+  if(DialogueController::getState() == 10) {
+    GameWindow::createText("Next", 292, 390 + (18 * 1), 544, 45, text_color_selected);
+  }
 }
 
-/*Removes all appearances of a specified char(charsToRemove) from a 
+/*Removes all appearances of a specified char(charsToRemove) from a
 specified string(str)*/
-std::string DialogueGui::remove_chars_from_string(string &str, char* charsToRemove) {
-	for (unsigned int i = 0; i < strlen(charsToRemove); ++i) {
-		str.erase(remove(str.begin(), str.end(), charsToRemove[i]), str.end());
-	}
-	return str;
+std::string DialogueGui::remove_chars_from_string(string &str, char* charsToRemove)
+{
+  for(unsigned int i = 0; i < strlen(charsToRemove); ++i) {
+    str.erase(remove(str.begin(), str.end(), charsToRemove[i]), str.end());
+  }
+  return str;
 }
 
 /*Replaces all appearances of a specified string(replace) with a
 specified char(ch)*/
-std::string DialogueGui::replace_str_char(string str, const string& replace, char ch) {
+std::string DialogueGui::replace_str_char(string str, const string& replace, char ch)
+{
 
-	// set our locator equal to the first appearance of any character in replace
-	size_t found = str.find_first_of(replace);
+// set our locator equal to the first appearance of any character in replace
+  size_t found = str.find_first_of(replace);
 
-	while (found != string::npos) { // While our position in the sting is in range.
-		str[found] = ch; // Change the character at position.
-		found = str.find_first_of(replace, found + 1); // Relocate again.
-	}
+  while(found != string::npos) { // While our position in the sting is in range.
+    str[found] = ch; // Change the character at position.
+    found = str.find_first_of(replace, found + 1); // Relocate again.
+  }
 
-	return str; // return our new string.
+  return str; // return our new string.
 }
--- ChildrenOfOsi/ChildrenOfOsi/DialogueGui.h
+++ ChildrenOfOsi/ChildrenOfOsi/DialogueGui.h
@@ -1,54 +1,108 @@
 #pragma once
+
 #include "common.h"
-#include "Rectangle.h"
-#include "GameWindow.h"
+
+#include "AssetInfo.h"
 #include "DialogueController.h"
+#include "GameWindow.h"
+#include "Rectangle.h"
 
 class DialogueGui
 {
+  private:
 
-private:
-	Rectangle* background;
-	Rectangle* swordIcon;
-	Rectangle* heartIcon;
-	Rectangle* faceIcon;
-	Rectangle* questionIcon;
-	Rectangle* dialogueBox;
-	Rectangle* responseBox1;
-	Rectangle* responseBox2;
-	Rectangle* portrait1;
-	Rectangle* portrait2;
+  static constexpr float BACKGROUND_X = 160.0F;
+  static constexpr float BACKGROUND_Y = 305.0F;
+  static constexpr float BACKGROUND_WIDTH = 640.0F;
+  static constexpr float BACKGROUND_HEIGHT = 225.0F;
 
-	//Texture
-	Texture* backgroundTex;
-	Texture* swordTex;
-	Texture* swordGlowTex;
-	Texture* heartTex;
-	Texture* heartGlowTex;
-	Texture* faceTex;
-	Texture* faceGlowTex;
-	Texture* questionTex;
-	Texture* questionGlowTex;
-	Texture* dialogueBoxTex;
-	Texture* responseBoxTex;
-	
-	glm::vec3 black;
-	glm::vec3 red;
+  static constexpr float HEADER_X = 355.0F;
+  static constexpr float HEADER_Y = 220.0F;
+  static constexpr float HEADER_WIDTH = 250.0F;
+  static constexpr float HEADER_HEIGHT = 75.0F;
 
-public:
-	DialogueGui();
-	~DialogueGui();
-	void loadTexture();
-	void setSprite();
-	void drawGui();
-	void setPortrait1(Sprite portrait1Sprite);
-	void setPortrait2(Sprite portrait2Sprite);
-	void setSwordGlow();
-	void setHeartGlow();
-	void setFaceGlow();
-	void setQuestionGlow();
-	std::string remove_chars_from_string(string &str, char* charsToRemove);
-	std::string replace_str_char(string str, const string& replace, char ch);
-	
+  static constexpr float ICON_SWORD_X = 0.0F;
+  static constexpr float ICON_SWORD_Y = 0.0F;
+  static constexpr float ICON_HEART_X = 0.0F;
+  static constexpr float ICON_HEART_Y = 0.0F;
+  static constexpr float ICON_FACE_X = 0.0F;
+  static constexpr float ICON_FACE_Y = 0.0F;
+  static constexpr float ICON_QUESTION_X = 0.0F;
+  static constexpr float ICON_QUESTION_Y = 0.0F;
+  static constexpr float ICON_WIDTH = 30.0F;
+  static constexpr float ICON_HEIGHT = 30.0F;
+
+  static constexpr float SPEAKER_LEFT_X = 0.0F;
+  static constexpr float SPEAKER_LEFT_Y = 0.0F;
+  static constexpr float SPEAKER_LEFT_WIDTH = 0.0F;
+  static constexpr float SPEAKER_LEFT_HEIGHT = 0.0F;
+
+  static constexpr float SPEAKER_RIGHT_X = 0.0F;
+  static constexpr float SPEAKER_RIGHT_Y = 0.0F;
+  static constexpr float SPEAKER_RIGHT_WIDTH = 0.0F;
+  static constexpr float SPEAKER_RIGHT_HEIGHT = 0.0F;
+
+  Rectangle *background_rect;
+  Rectangle *header_rect;
+  Rectangle *icon_sword_rect;
+  Rectangle *icon_heart_rect;
+  Rectangle *icon_face_rect;
+  Rectangle *icon_question_rect;
+  Rectangle *speaker_left_rect;
+  Rectangle *speaker_right_rect;
+
+  Texture *background_tex;
+  Texture *header_tex;
+  Texture *icon_sword_tex;
+  Texture *icon_heart_tex;
+  Texture *icon_face_tex;
+  Texture *icon_question_tex;
+  Texture *speaker_left_tex;
+  Texture *speaker_right_tex;
+
+  Rectangle* background;
+  Rectangle* swordIcon;
+  Rectangle* heartIcon;
+  Rectangle* faceIcon;
+  Rectangle* questionIcon;
+  Rectangle* dialogueBox;
+  Rectangle* responseBox1;
+  Rectangle* responseBox2;
+  Rectangle* portrait1;
+  Rectangle* portrait2;
+
+  Texture* backgroundTex;
+  Texture* swordTex;
+  Texture* swordGlowTex;
+  Texture* heartTex;
+  Texture* heartGlowTex;
+  Texture* faceTex;
+  Texture* faceGlowTex;
+  Texture* questionTex;
+  Texture* questionGlowTex;
+  Texture* dialogueBoxTex;
+  Texture* responseBoxTex;
+
+  glm::vec3 text_color_default;
+  glm::vec3 text_color_selected;
+
+  public:
+
+  DialogueGui();
+  ~DialogueGui();
+
+  void loadTexture();
+  void setSprite();
+  void drawGui();
+
+  void setPortrait1(Sprite portrait1Sprite);
+  void setPortrait2(Sprite portrait2Sprite);
+
+  void setSwordGlow();
+  void setHeartGlow();
+  void setFaceGlow();
+  void setQuestionGlow();
+
+  std::string remove_chars_from_string(string &str, char* charsToRemove);
+  std::string replace_str_char(string str, const string& replace, char ch);
 };
-
