13f1e1b244eeaad65f836dad94979ca988bd2181
Daniel Hildebrandt <daniel.s.hildebrandt+git@gmail.com>
2017-05-08 11:52:55 -0700

    New Oya sprites mostly implemented, minus a bug with her right-death animation.

--- ChildrenOfOsi/ChildrenOfOsi/AssetInfo.h
+++ ChildrenOfOsi/ChildrenOfOsi/AssetInfo.h
@@ -8,6 +8,8 @@ const std::string FONTS_PATH = ASSETS_PATH + "Fonts/";
 
 const std::string SPRITES_PATH = ASSETS_PATH + "Sprites/";
 const std::string CHARACTER_SPRITES_PATH = SPRITES_PATH + "Characters/";
+
+const std::string SHANGO_PATH = CHARACTER_SPRITES_PATH + "Shango/";
 const std::string YEMOJA_PATH = CHARACTER_SPRITES_PATH + "Yemoja/";
 const std::string OYA_PATH = CHARACTER_SPRITES_PATH + "Oya/";
 const std::string OSHOSI_PATH = CHARACTER_SPRITES_PATH + "Oshosi/";
