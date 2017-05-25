40bbef19642a5143edb49b2c4b06021ed2023eb9
U-DESKTOP-IJO7N67\BlackMagicianDTT <dtoney@ucsc.edu>
2017-05-25 13:37:09 -0700

    Added dialog json files for shrines, babalawos and villagers.

--- ChildrenOfOsi/ChildrenOfOsi/DialogueController.cpp
+++ ChildrenOfOsi/ChildrenOfOsi/DialogueController.cpp
@@ -1552,15 +1552,15 @@ void DialogueController::startConversation(WorldObj* n, bool playerTalk)
 	Hero* temp_hero = CheckClass::isHero(other);
 	std::string start_message = "";
 
-	/*if (temp_hero) {
+	if (temp_hero) {
 		player->filter_move_to(temp_hero);
 		//if (first_buff) {
-			temp_hero->rel[player->name]->addNotoriety(25);
+			temp_hero->rel[player->name]->addNotoriety(50);
 			temp_hero->rel[player->name]->addStrength(50);
-			temp_hero->rel[player->name]->addAffinity(20);
+			temp_hero->rel[player->name]->addAffinity(50);
 		//}
 		//first_buff = false;
-	}*/
+	}
 	/*handles what the greeting phrase should be based on whether or not the player
 	has taken or completed a quest from the npc*/
 		if (temp_hero) {
