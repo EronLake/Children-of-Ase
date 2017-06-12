1144fe2fee490345a548d5b6f91fdc0d8b5890ae
Andrew Patrick Barr <anpbarr@crm.ucsc.edu>
2017-06-11 19:48:43 -0700

    diminishing returns is fixed, tested, and working as expected

--- ChildrenOfOsi/ChildrenOfOsi/GameWindow.cpp
+++ ChildrenOfOsi/ChildrenOfOsi/GameWindow.cpp
@@ -324,7 +324,7 @@ void GameWindow::setupWindow()
   glfwInit();
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
   glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
 
   GameWindow::primaryMonitor = glfwGetPrimaryMonitor();
